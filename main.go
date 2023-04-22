2af46c5f1fc687a3a893b39ad31033e81554372a
kristof1345 <kristofnemeth1345@gmail.com>
2023-04-22 21:33:16 +0200

    nk

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kristof1345/cards"
+)
+
+type Hand []cards.Card
+
+// separate them with a comma
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
+func main() {
+	deck := cards.New(cards.Deck(3), cards.Shuffle)
+	var card cards.Card
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, deck = draw(deck)
+			*hand = append(*hand, card)
+		}
+	}
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, deck = draw(deck)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", player)
+	fmt.Println("Dealer:", dealer)
+}
+
+func draw(deck []cards.Card) (cards.Card, []cards.Card) {
+	return deck[0], deck[1:]
+}
